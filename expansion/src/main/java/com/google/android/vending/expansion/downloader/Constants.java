b9f111780762d4884da688b314267b4a7126e654
Alexey Danilov <danikula@gmail.com>
2016-04-07 13:35:25 +0300

    increase max redirects count

--- expansion/src/main/java/com/google/android/vending/expansion/downloader/Constants.java
+++ expansion/src/main/java/com/google/android/vending/expansion/downloader/Constants.java
@@ -87,7 +87,7 @@ public class Constants {
     /**
      * The maximum number of redirects.
      */
-    public static final int MAX_REDIRECTS = 5; // can't be more than 7.
+    public static final int MAX_REDIRECTS = 10;
 
     /**
      * The time between a failure and the first retry after an IOException.
